34a3e4f52a672322092ac8884ac5fddce6ae01fc
Faiza Azizan <faizadraws@gmail.com>
2022-09-13 16:58:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import pandas
 
 streamlit.title('Sample Text')
 fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Select some fruits: ", list(fruits.index))
 streamlit.dataframe(fruits)
