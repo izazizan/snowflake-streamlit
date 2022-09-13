b4bcf773945688d94bf022e4382beb1b68bf3905
Faiza Azizan <faizadraws@gmail.com>
2022-09-13 16:54:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
-import streamlit;
+import streamlit
+import pandas
 
-streamlit.title('Sample Text');
+streamlit.title('Sample Text')
+fruits = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(fruits)
